eacf2bf4679790c87447bdbc552c3a8fddad8eb2
RayChen <53119155+RayChen1996@users.noreply.github.com>
2024-05-22 22:44:09 +0800

    添加素材檔 + 主色 + UI 庫

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,8 +13,32 @@ const config: Config = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+      colors: {
+        primary: {
+          100: "#BAE5FF",
+          200: "#70C0EF",
+          300: "#1E4E6C",
+        },
+        accent: {
+          orange: "#ED9C5A",
+          green: "#24948E",
+        },
+        background: "#EEF0F3",
+        white: {
+          DEFAULT: "#FAFAFA",
+          dark: "#131313",
+        },
+        gray: {
+          100: "#EDEDED",
+          200: "#D1D1D1",
+          300: "#B6B6B6",
+          400: "#919191",
+          500: "#626262",
+          600: "#3A3A3A",
+        },
+      },
     },
   },
-  plugins: [],
+  plugins: [require("daisyui")],
 };
 export default config;
