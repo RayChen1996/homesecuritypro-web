3676509a5b2b6f6033c999c5ef9e7d3ba3e09590
RayChen1996 <53119155+RayChen1996@users.noreply.github.com>
2024-06-07 08:32:31 +0800

    +new 圖

--- src/components/pages/home/Carousel.tsx
+++ src/components/pages/home/Carousel.tsx
@@ -2,6 +2,8 @@
 import React from "react";
 import Image from "next/image";
 import swiperImage from "@/../../public/image (1).png";
+import swiperImage2 from "@/../../public/earthquake.png";
+import swiperImage3 from "@/../../public/earthquake2.png";
 import dbArrowSvg from "@/../../public/dbArrow.svg";
 import Link from "next/link";
 import { Swiper, SwiperSlide } from "swiper/react";
@@ -30,7 +32,29 @@ export default function Carousel() {
             sizes="(min-width: 1024px) 70vw, 100vw"
             className="object-cover"
           />
-          <div className=" m-auto  mt-80 h-1/5  w-1/5    rounded-xl flex justify-center items-center p-12 flex-col top-1/3 bg-primary-300 opacity-75 ">
+          <span className=" m-auto  mt-80 h-1/5   w-1/4   rounded-xl flex justify-center items-center p-12 flex-col top-1/3 bg-primary-300 opacity-75 ">
+            <h3 className=" text-white text-[40px] font-bold">
+              您的家安全嗎？
+            </h3>
+            <Link href={"/"} className=" flex items-center">
+              <span className=" text-white text-2xl font-bold">
+                找專家，保護家
+              </span>
+
+              <Image alt="" src={dbArrowSvg} width={40} height={40} />
+            </Link>
+          </span>
+        </SwiperSlide>
+
+        <SwiperSlide>
+          <Image
+            alt=""
+            src={swiperImage3}
+            fill
+            sizes="(min-width: 1024px) 70vw, 100vw"
+            className="object-cover"
+          />
+          <div className=" m-auto  mt-80 h-1/5   w-1/4   rounded-xl flex justify-center items-center p-12 flex-col top-1/3 bg-primary-300 opacity-75 ">
             <h3 className=" text-white text-[40px] font-bold">
               您的家安全嗎？
             </h3>
@@ -43,15 +67,16 @@ export default function Carousel() {
             </Link>
           </div>
         </SwiperSlide>
+
         <SwiperSlide>
           <Image
             alt=""
-            src={swiperImage}
+            src={swiperImage2}
             fill
             sizes="(min-width: 1024px) 70vw, 100vw"
             className="object-cover"
           />
-          <div className=" m-auto  mt-80 h-1/5  w-1/5    rounded-xl flex justify-center items-center p-12 flex-col top-1/3 bg-primary-300 opacity-75 ">
+          <div className=" m-auto  mt-80 h-1/5  w-1/4  rounded-xl flex justify-center items-center p-12 flex-col top-1/3 bg-primary-300 opacity-75 ">
             <h3 className=" text-white text-[40px] font-bold">
               您的家安全嗎？
             </h3>
